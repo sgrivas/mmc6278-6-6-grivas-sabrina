1fa8c9fb056bf9de507ca588a9c92febab4de774
Sabrina Grivas <sgrivas1@gmail.com>
2022-06-15 07:51:50 -0400

    toggling aria attributes

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 var hamburgerButton=document.querySelector('nav button');
 var hamburgerMenu=document.getElementById('main-menu')
+var menu=document.querySelector('nav')
+
 hamburgerButton.addEventListener('click', function(){
-    hamburgerMenu.classList.toggle('show-menu')
-    hamburgerButton.setAttribute('aria-expanded', 'true')
+    if (hamburgerMenu.classList.contains('show-menu')) {
+        closeMenu()
+    } else{
+        openMenu()
+    }
 })
 
 document.addEventListener('keyup', function(event) {      
     if (event.key === 'Escape' && hamburgerMenu.classList.contains('show-menu')){
-        hamburgerMenu.classList.remove('show-menu')
-        hamburgerButton.focus();
+        closeMenu();
     }
 });
 
-if (!(hamburgerMenu.classList.contains('show-menu'))) {
+function openMenu(){
+    hamburgerMenu.classList.add('show-menu')
+    hamburgerButton.setAttribute('aria-expanded', 'true')
+}
+
+function closeMenu() {
+    hamburgerMenu.classList.remove('show-menu')
+    hamburgerButton.focus();
     hamburgerButton.setAttribute('aria-expanded', 'false')
 }
+
+document.body.addEventListener('click', function(e){
+    if (menu.contains(e.target)) {
+        return
+      }
+      closeMenu()
+})
